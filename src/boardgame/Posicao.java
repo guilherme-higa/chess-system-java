8b065677750ca5805b90757cf88ef95b45bed0e2
Guilherme Higa <guilherme.higa.si@gmail.com>
2021-08-17 17:58:17 -0300

    Primeira classe : Posição

---
+++ src/boardgame/Posicao.java
@@ -0,0 +1,39 @@
+package boardgame;
+
+public class Posicao {
+
+	private int row;
+	private int column;
+
+	public Posicao() {
+
+	}
+
+	public Posicao(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(row + "," + column);
+		return sb.toString();
+	}
+}
